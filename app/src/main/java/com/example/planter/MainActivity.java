e30d071bb3ccb784dccc1b1fc88fe0cfce61e2fe
arian-etfocean <arian@etfocean.com>
2021-06-24 11:09:25 -0400

    plant type list added, photos name updated

--- app/src/main/java/com/example/planter/MainActivity.java
+++ app/src/main/java/com/example/planter/MainActivity.java
@@ -11,7 +11,13 @@ import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.karumi.dexter.Dexter;
@@ -24,6 +30,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Date;
@@ -37,9 +44,12 @@ public class MainActivity extends AppCompatActivity {
     private static final int MY_CAMERA_REQUEST_CODE = 7171;
 
     @BindView(R.id.btn_camera)
-    ImageView btn_camera;
+    Button btn_camera;
     @BindView(R.id.img_preview)
     ImageView img_preview;
+
+    @BindView(R.id.txt_plant)
+    TextView plantType;
 
     private Uri imageUri;
 
@@ -49,8 +59,7 @@ public class MainActivity extends AppCompatActivity {
                 .withPermissions(Arrays.asList(
                         Manifest.permission.WRITE_EXTERNAL_STORAGE,
                         Manifest.permission.READ_EXTERNAL_STORAGE,
-                        Manifest.permission.CAMERA
-                        ))
+                        Manifest.permission.CAMERA))
                 .withListener(new MultiplePermissionsListener() {
                     @Override
                     public void onPermissionsChecked(MultiplePermissionsReport multiplePermissionsReport) {
@@ -61,8 +70,8 @@ public class MainActivity extends AppCompatActivity {
                             DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd hh:m:ss");
                             String strDate = dateFormat.format(date);
 
-
-                            contentValues.put(MediaStore.Images.Media.TITLE, "NewPicture_"+strDate);
+                            String plant = plantType.getText().toString();
+                            contentValues.put(MediaStore.Images.Media.TITLE, plant+'_'+strDate);
                             contentValues.put(MediaStore.Images.Media.DESCRIPTION, "From your camera");
                             imageUri = getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues);
                             Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
@@ -102,5 +111,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
+        TextView plantType = findViewById(R.id.txt_plant);
+
+        final ListView list = findViewById(R.id.list_plant);
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add("Kale");
+        arrayList.add("Basil");
+        arrayList.add("Lettuce");
+        arrayList.add("Asparagus");
+        arrayList.add("Mint");
+        arrayList.add("Spinach");
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, arrayList);
+        list.setAdapter(arrayAdapter);
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String clickedItem=(String) list.getItemAtPosition(position);
+                plantType.setText("Plant Type " + clickedItem);
+//                Toast.makeText(MainActivity.this,test,Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
